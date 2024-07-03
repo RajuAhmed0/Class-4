f6bf0c67bbf86820428d75d8bbfa6a4a39b9c132
Your Name <you@example.com>
2024-07-03 14:01:42 +0600

    task complate

---
+++ all-task.js
@@ -0,0 +1,20 @@
+/* Task - 1 */
+function Foo() {
+   console.log("foo"); Bar();
+    
+}
+
+function Bar() {
+   console.log("bar"); 
+}
+Foo();
+Bar("foo");
+
+
+
+
+/* function foo() {console.log("foo");bar ();} 
+function bar() {console.log("bar");}foo ();
+
+bar('foo')
+foo() */
