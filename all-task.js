155c34fa811d4642d09653f76ecc17ced7a6e1a6
Your Name <you@example.com>
2024-07-03 14:54:37 +0600

    file added

--- all-task.js
+++ all-task.js
@@ -13,3 +13,27 @@ Foo();
 
 
 /* Task - 2  */
+function make_avg (num1, num2, num3) {
+   var total = num1 + num2 + num3;
+   var average = total / 3;
+   return average;  
+}
+ var avg = make_avg (103, 105, 112);
+ console.log(avg);
+
+ 
+
+/*  Task - 3 */
+// function make_avgs (num1, num2, num3, num4, num5) {
+//    var totalAmount = num1 + num2 + num3 + num4 + num5;
+//    var averages = totalAmount / 5;
+//    return averages;  
+// }
+//  var avg = make_avgs (10, 12, 15, 23, 35);
+//  console.log(avg);
+
+
+
+
+
+
