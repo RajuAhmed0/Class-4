e508c66c0bb298766f20b85f46418894de4a26be
Your Name <you@example.com>
2024-07-03 15:16:30 +0600

    task all complate

--- all-task.js
+++ all-task.js
@@ -1,11 +1,11 @@
 /* Task - 1 */
 function Foo() {
    console.log("foo"); Bar();
-    
+
 }
 
 function Bar() {
-   console.log("bar"); 
+   console.log("bar");
 }
 
 Bar("foo");
@@ -13,15 +13,15 @@ Foo();
 
 
 /* Task - 2  */
-function make_avg (num1, num2, num3) {
+function make_avg(num1, num2, num3) {
    var total = num1 + num2 + num3;
    var average = total / 3;
-   return average;  
+   return average;
 }
- var avg = make_avg (103, 105, 112);
- console.log(avg);
+var avg = make_avg(103, 105, 112);
+console.log(avg);
 
- 
+
 
 /*  Task - 3 */
 // function make_avgs (num1, num2, num3, num4, num5) {
@@ -32,6 +32,27 @@ function make_avg (num1, num2, num3) {
 //  var avg = make_avgs (10, 12, 15, 23, 35);
 //  console.log(avg);
 
+/* Task - 4 */
+
+
+
+
+
+
+
+
+/* Task - 5 */
+var color = "Green";
+
+switch (color) {
+   case 'Red': console.log('Don,t crossing road');
+      break;
+   case 'Yellow': console.log('Stop and waiting green signal');
+      break;
+   case 'Green': console.log('Signal green cross the road');
+      break;
+   default: console.log('Flow the Traffic signal and cross the road');
+}
 
 
 
